367c6f12227ee1b24381452e19e4f4f399743e86
Wang Rensheng <37458584+RenshengWang@users.noreply.github.com>
2018-05-11 20:14:44 -0400

    Update rootlogon.C

--- rootlogon.C
+++ rootlogon.C
@@ -1,6 +1,7 @@
 {
 //this will load the class "ESpec_Fit_Err".
-//with single '+', it means that if this class is modified, then the root will compiled this class automatically.
-//with two '+', it means that regardless of modificationroot will compiled this class automatically.
+//with single '+': if this class is modified, the root will compiled this class automatically.
+//with two '+': regardless of modification, the root will compile this class automatically.
+//without '+': the root will load the library of this class, and never compile it.
   gROOT->ProcessLine(".L ESpec_Fit_Err.cpp+");
 }
